c24b8ef3eec6f67f721d73ab2290173c00298470
Loretta <101978292+Rietta1@users.noreply.github.com>
2023-09-30 09:51:26 +0100

    #25 refactor s3 bucket into the new terrahouse_aws module (#26)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -1,4 +1,5 @@
 variable "user_uuid" {
+  description = "The UUID of the user"
   type        = string
   validation {
     condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
@@ -7,6 +8,7 @@ variable "user_uuid" {
 }
 
 variable "bucket_name" {
+  description = "The name of the S3 bucket"
   type        = string
 
   validation {
